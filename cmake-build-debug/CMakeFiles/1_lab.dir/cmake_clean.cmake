89057ffac22815fb0d87ac60c4d2862795c2c531
AlexHarlamov <a.kharlamov1@g.nsu.ru>
2018-10-17 20:42:07 +0700

    none

---
+++ cmake-build-debug/CMakeFiles/1_lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1_lab.dir/main.cpp.obj"
+  "1_lab.pdb"
+  "1_lab.exe"
+  "1_lab.exe.manifest"
+  "lib1_lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
