bd8ae56a7c827955b1beb4dd23297be9d7859a48
AlexHarlamov <a.kharlamov1@g.nsu.ru>
2018-11-21 15:14:56 +0700

    ok

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -12,14 +12,26 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.12.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.12.1/CMakeRCCompiler.cmake"
   "CMakeFiles/3.12.1/CMakeSystem.cmake"
+  "../lib/googletest-master/CMakeLists.txt"
+  "../lib/googletest-master/googlemock/CMakeLists.txt"
+  "../lib/googletest-master/googlemock/cmake/gmock.pc.in"
+  "../lib/googletest-master/googlemock/cmake/gmock_main.pc.in"
+  "../lib/googletest-master/googletest/CMakeLists.txt"
+  "../lib/googletest-master/googletest/cmake/Config.cmake.in"
+  "../lib/googletest-master/googletest/cmake/gtest.pc.in"
+  "../lib/googletest-master/googletest/cmake/gtest_main.pc.in"
+  "../lib/googletest-master/googletest/cmake/internal_utils.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/BasicConfigVersion-AnyNewerVersion.cmake.in"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeCInformation.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeCXXInformation.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeCommonLanguageInclude.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeDependentOption.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeExtraGeneratorDetermineCompilerMacrosAndIncludeDirs.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeFindCodeBlocks.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeGenericSystem.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeInitializeConfigs.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeLanguageInformation.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakePackageConfigHelpers.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeRCInformation.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeSystemSpecificInformation.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/CMakeSystemSpecificInitialize.cmake"
@@ -27,6 +39,10 @@ set(CMAKE_MAKEFILE_DEPENDS
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Compiler/GNU-C.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Compiler/GNU-CXX.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Compiler/GNU.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/FindPackageHandleStandardArgs.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/FindPackageMessage.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/FindPythonInterp.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/GNUInstallDirs.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Platform/Windows-GNU-C-ABI.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Platform/Windows-GNU-C.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Platform/Windows-GNU-CXX-ABI.cmake"
@@ -36,6 +52,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Platform/Windows.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/Platform/WindowsPaths.cmake"
   "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/ProcessorCount.cmake"
+  "D:/Program Files/CLion 2018.2.3/bin/cmake/win/share/cmake-3.12/Modules/WriteBasicConfigVersionFile.cmake"
   )
 
 # The corresponding makefile is:
@@ -47,9 +64,16 @@ set(CMAKE_MAKEFILE_OUTPUTS
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/CMakeDirectoryInformation.cmake"
+  "lib/googletest-master/CMakeFiles/CMakeDirectoryInformation.cmake"
+  "lib/googletest-master/googlemock/CMakeFiles/CMakeDirectoryInformation.cmake"
+  "lib/googletest-master/googlemock/gtest/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/1_lab.dir/DependInfo.cmake"
+  "lib/googletest-master/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake"
+  "lib/googletest-master/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
   )
